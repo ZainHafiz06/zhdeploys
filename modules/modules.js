7b46bda73a0b4af883489c8bb4f8b575ca988237
Zain <knight200699@gmail.com>
2025-11-04 21:52:53 -0600

    Vite Integration

--- modules/modules.js
+++
@@ -1,3 +0,0 @@
-export function modules_wrpd() {
-
-}
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { components_wrpd } from '../components/components.js';
-import { modules_wrpd } from '../modules/modules.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    components_wrpd();
-    modules_wrpd();
-});
