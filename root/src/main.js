103f4d7f5d974315d7c877e9fe416f4acc205ecf
Zain <knight200699@gmail.com>
2025-11-04 20:12:14 -0600

    Boilerplate

---
+++ root/src/main.js
@@ -0,0 +1,7 @@
+import { components_wrpd } from '../components/components.js';
+import { modules_wrpd } from '../modules/modules.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    components_wrpd();
+    modules_wrpd();
+});
