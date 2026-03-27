90af5eb302edea2310b9d5194ecbad794e0a8938
ZainHafiz06 <knight200699@gmail.com>
2026-03-26 20:40:20 -0500

    zhdeploys.V1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="s-container">
+                <video 
+                    className="ambient-video"
+                    autoPlay 
+                    muted 
+                    loop 
+                    playsInline
+                >
+                    <source src="/clip.mp4" type="video/mp4" />
+                </video>
+
+                <video 
+                    className="main-video"
+                    autoPlay 
+                    muted 
+                    loop 
+                    playsInline
+                >
+                    <source src="src/assets/clip.mp4" type="video/mp4" />
+                </video>
+
+                <h1 className="h-1">
+                    code that actually ships.
+                </h1>
+
+                <h1 className="h-2">
+                    <a href="https://github.com/ZainHafiz06/Portfolio">
+                        https://github.com/ZainHafiz06/Portfolio
+                    </a>
+                </h1>                 
+            </div>
+        </div>
+    )
+}
